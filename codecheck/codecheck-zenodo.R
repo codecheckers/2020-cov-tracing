6a39184b1fa2449f1a4ff9d67b4ff874cefbdd9b
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-27 09:45:10 +0100

    update doc for zenodo upload.

--- codecheck/codecheck-zenodo.R
+++ codecheck/codecheck-zenodo.R
@@ -1,9 +1,6 @@
-## Interactive script for Zenodo deposit.
-## This file should live in the codecheck/ folder.
-## [2020-04-19 Sun] -- uses codecheckers/codecheck package.
+## Zenodo deposit; see vignette("codecheck_overview.Rmd")
 
 require(codecheck)
-
 metadata = read_yaml( "../codecheck.yml") 
 
 ## To interact with the Zenodo API, you need to create a token.  This should
@@ -15,11 +12,13 @@ my_token = system("pass show codechecker-token", intern=TRUE)
 zenodo <- ZenodoManager$new(token = my_token)
 
 
-## If you wish to create a new record on zenodo, run the following line once
-## and then store the URL of the record in  ../codecheck.yml
+## To create a new Zenodo record, run the following line ONCE
+## and update the report: line of  ../codecheck.yml
 ## This will generate a new record every time that you run it.
+## After creating the record, reload the metadata above.
+##
 ## record = create_zenodo_record(zenodo)
-## you will then to reload the yml
+
 
 record = get_zenodo_record(metadata$report)
 set_zenodo_metadata(zenodo, record, metadata)
